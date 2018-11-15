85746b7668562738736729c1847965690eb3478f
wbrooks <wibrook686@gmail.com>
2018-11-15 15:13:20 -0700

    fixed airlines

--- Extravelocity/src/software/Website.java
+++ Extravelocity/src/software/Website.java
@@ -1,6 +1,7 @@
 package software;
 
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 import java.io.*;
 import java.time.LocalDateTime;
 import users.*;
@@ -463,7 +464,7 @@ public class Website {
 				for (int k = 0; k < todo.get(j).getConnections().size(); k++) {
 					for (int l = 0; l < todo.get(j).getConnections().get(k).getFlightsPerDay(); l++) {
 						new Flight(todo.get(j), todo.get(j).getConnections().get(k).getDestination(), 
-								todo.get(j).getAirlineList().get((int)Math.random() * todo.get(j).getAirlineList().size()), craft, 
+								todo.get(j).getAirlineList().get(ThreadLocalRandom.current().nextInt(0, todo.get(j).getAirlineList().size())), craft, 
 								aDate.plusHours((long) (Math.random() * 24)), "111", 300, 0);
 						
 					}
